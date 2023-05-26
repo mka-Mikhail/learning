ea10333b47121df63d9f2220e943755e12b845a7
mka-Mikhail <mixailrojnow@yandex.ru>
2023-05-26 09:40:41 +0300

    ещё одна реализация форматирования числа

--- src/main/java/lesson4/homework/formatting/Formatting.java
+++ src/main/java/lesson4/homework/formatting/Formatting.java
@@ -8,11 +8,15 @@ import java.util.Locale;
 public class Formatting {
     public static void main(String[] args) {
         double value = 123487655.67;
-        System.out.printf("$%,.2f\n", value);
         DecimalFormatSymbols symbols = DecimalFormatSymbols.getInstance();
         symbols.setGroupingSeparator(',');
         DecimalFormat df = new DecimalFormat("###,###.##", symbols);
         System.out.println("$" + df.format(value));
         System.out.println("$" + NumberFormat.getNumberInstance(new Locale.Builder().setLanguage("en").setRegion("US").build()).format(value));
+
+        Locale locale = Locale.US;
+        String ptn = "$%,.2f";
+        String result = String.format(locale, ptn, value);
+        System.out.println(result);
     }
 }
